1edd585b521895728fd79f3fbe29fea8f7702479
Sonia Ly <lysoksonia@gmail.com>
2023-11-02 14:36:57 -0400

    Ajouté la enum assets et les assets dans le dossier "assets"

--- src/main/java/ca/qc/bdeb/sim203/projetjavafx/Charlotte.java
+++ src/main/java/ca/qc/bdeb/sim203/projetjavafx/Charlotte.java
@@ -1,4 +1,26 @@
 package ca.qc.bdeb.sim203.projetjavafx;
 
+import javafx.scene.canvas.*;
+import javafx.scene.image.*;
+
 public class Charlotte {
+    private double w = 102;
+    private double h = 90;
+
+    private double x, y;
+
+    private Image image = new Image("src/assets/charlotte");
+
+    public Charlotte() {
+    }
+
+    public void update(double deltaTemps) {
+
+    }
+
+    public void draw(GraphicsContext context) {
+        context.drawImage(image, w, h);
+    }
+
+
 }
--- src/main/java/ca/qc/bdeb/sim203/projetjavafx/Main.java
+++ src/main/java/ca/qc/bdeb/sim203/projetjavafx/Main.java
@@ -1,6 +1,10 @@
 package ca.qc.bdeb.sim203.projetjavafx;
 
+import javafx.animation.*;
 import javafx.application.Application;
+import javafx.scene.*;
+import javafx.scene.canvas.*;
+import javafx.scene.layout.*;
 import javafx.stage.*;
 
 public class Main extends Application {
@@ -8,10 +12,40 @@ public class Main extends Application {
         launch(args);
     }
 
+    public static int WIDTH = 900;
+    public static int HEIGHT = 520;
     @Override
-    public void start(Stage primaryStage) {
-        System.out.println("Hello World!");
-        System.out.println("Salut Camille");
+    public void start(Stage primaryStage) throws Exception {
 
+        //region -- VARIABLES D'AFFICHAGE DE BASE --
+        var root = new Pane();
+
+        // Une scene, canvas et un context de base juste pour tester Charlotte qui bouge
+        var scene = new Scene(root, WIDTH, HEIGHT);
+        var canvas = new Canvas(WIDTH, HEIGHT);
+        var context = canvas.getGraphicsContext2D();
+        root.getChildren().add(canvas);
+        //endregion
+
+        Charlotte charlotte = new Charlotte();
+
+        AnimationTimer timer = new AnimationTimer() {
+
+            long lastTime = System.nanoTime();
+            @Override
+            public void handle(long now) {
+                double deltaTemps = (now - lastTime) * 1e-9;
+                //region -- DRAW --
+                context.clearRect(0, 0, WIDTH, HEIGHT);
+                charlotte.draw(context);
+                //endregion
+            }
+        };
+
+        timer.start();
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Charlotte la Barbotte");
+        primaryStage.show();
     }
 }
