d002d02948c11566f1428180203d66da470c51ae
Sonia Ly <lysoksonia@gmail.com>
2023-11-03 09:09:44 -0400

    Fix temporaire: Charlotte ne décélère pas, elle elle s'arrête abruptement.

---
+++ src/main/java/ca/qc/bdeb/sim203/projetjavafx/Scenes.java
@@ -0,0 +1,14 @@
+package ca.qc.bdeb.sim203.projetjavafx;
+
+import javafx.scene.*;
+
+public class Scenes {
+
+    public Scenes ecranAccueil() {
+        var ecranAccueil = new Scenes();
+
+
+
+        return ecranAccueil;
+    }
+}
