9a6c52affc18ecb67da565ba4a75ba23383be8a6
Camille Marquis <camimarquis@gmail.com>
2023-11-04 12:41:05 -0400

    scene d'accueil qui n'Est pas centered L camille

--- src/main/java/ca/qc/bdeb/sim203/projetjavafx/SceneAccueil.java
+++ src/main/java/ca/qc/bdeb/sim203/projetjavafx/SceneAccueil.java
@@ -7,18 +7,22 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+import static javafx.scene.paint.Color.BLUE;
 
 
 public class SceneAccueil extends Scenes{
 
-    private Button jouer = new Button("Jouer!");
-    private Button infos = new Button("Infos!");
 
-    private Image logo = new Image("logo.png");
 
 
     @Override
     public void construireScene() {
+        Image logo = new Image("logo.png");
+        Button infos = new Button("Infos!");
+        Button jouer = new Button("Jouer!");
+
         //section de boutons
         var groupeBoutons = new HBox();
         groupeBoutons.getChildren().add(jouer);
@@ -31,9 +35,14 @@ public class SceneAccueil extends Scenes{
         imgvLogo.setFitWidth(logo.getWidth()/1.5);
 
         var vboxAccueil = new VBox();
+        root.setStyle("-fx-background-color: #2A7FFF;");
+        vboxAccueil.setMaxHeight(HAUTEUR);
+        vboxAccueil.setMaxWidth(LARGEUR);
+
         vboxAccueil.getChildren().add(imgvLogo);
         vboxAccueil.getChildren().add(groupeBoutons);
         vboxAccueil.setAlignment(Pos.CENTER);
+
 
         root.getChildren().add(vboxAccueil);
 
