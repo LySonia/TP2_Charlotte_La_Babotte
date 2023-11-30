a85be00f66bb9869ca632939b1887d55677eb4a3
Camille Marquis <camimarquis@gmail.com>
2023-11-29 19:39:41 -0500

    javadocs pour étoile de mer

--- src/main/java/ca/qc/bdeb/sim203/projetjavafx/EtoileDeMer.java
+++ src/main/java/ca/qc/bdeb/sim203/projetjavafx/EtoileDeMer.java
@@ -2,7 +2,13 @@ package ca.qc.bdeb.sim203.projetjavafx;
 
 import javafx.scene.image.Image;
 
+
 public class EtoileDeMer extends Projectile{
+    /**
+     * Constructeur créant le projectile étoile de mer
+     * @param charlotte charlotte pour trouver la position initiale du projectile (centre de Charlotte)
+     * @param momentTire le moment exact où le projectile a été tirer
+     */
     public EtoileDeMer(Charlotte charlotte, double momentTire) {
         super(charlotte, momentTire);
         image = new Image(Assets.ETOILE.getEmplacement());
