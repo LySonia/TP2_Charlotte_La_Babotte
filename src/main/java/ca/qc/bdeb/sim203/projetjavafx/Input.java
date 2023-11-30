7678047a70325e0c4a5a73df7cb60bb2a7dbeba3
Camille Marquis <camimarquis@gmail.com>
2023-11-29 21:36:33 -0500

    javadocs pour input

--- src/main/java/ca/qc/bdeb/sim203/projetjavafx/Input.java
+++ src/main/java/ca/qc/bdeb/sim203/projetjavafx/Input.java
@@ -10,18 +10,29 @@ import javafx.scene.input.*;
 import java.util.*;
 
 public class Input {
-    //Associe chaque touche du clavier qui a été touché par l'utilisateur à "true" ou à "false"
-    //dépendant de si l'utilisateur pèse sur la touche ou non
+    /**
+     *   Associe chaque touche du clavier qui a été touché par l'utilisateur à "true" ou à "false"
+     *   dépendant de si l'utilisateur pèse sur la touche ou non
+     */
     public static HashMap<KeyCode, Boolean> touches = new HashMap<>();
 
+    /**
+     * Méthode qui va chercher la touche du clavier. Retourne la valeur du booléan si la clé est dans le HashMap.
+     * Sinon, si la clé ne se retrouve pas dans le HashMap, retourne false par défaut.
+     * @param touche le code de la touche appuyée
+     * @return boolean true si la clé est dans le hashmap
+     */
     public static boolean isKeyPressed(KeyCode touche) {
-        //Va chercher la touche du clavier. Retourne la valeur du booléan si la clé est dans le HashMap.
-        //Sinon, si la clé ne se retrouve pas dans le HashMap, retourne false par défaut.
+
         return touches.getOrDefault(touche, false);
     }
 
+    /**
+     * Mettre/ajuster la touche dans le Hashmap avec son boolean associé
+     * @param touche le code de la touche appuyé
+     * @param estAppuyee boolean si la touche est appuyée ou non
+     */
     public static void setKeyPressed(KeyCode touche, boolean estAppuyee) {
-        //Mettre/ajuster la touche dans le Hashmap avec son boolean associé
         touches.put(touche, estAppuyee);
     }
 
